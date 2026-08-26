13ee6abab3d9a019dfe2d762e327cde1e9bfc6f6
rterminus <luizrenanpontes@gmail.com>
2026-08-26 08:15:26 -0300

    chore: Character class renamed to Adventurer

---
+++ src/main/java/com/rpgadventure/Adventurer.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Adventurer {
+  private String name;
+  private int energy;
+  private int level;
+  private int coins;
+  private Inventory inventory;
+
+  public Adventurer(String name, int coins) {
+    this.name = name;
+    this.coins = coins;
+    this.level = 1;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getLevel() {
+    return level;
+  }
+
+  public int getCoins() {
+    return this.coins;
+  }
+
+  public int getEnergy() {
+    return this.energy;
+  }
+
+  public Inventory getInventory() {
+    return inventory;
+  }
+
+  public void dialogue() {
+    System.out.println("Talking...");
+  }
+
+  public void receive(Reward r) {
+    this.coins += r.getCoin();
+  }
+}
