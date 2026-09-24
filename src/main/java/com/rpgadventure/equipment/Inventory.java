04e39249b67f32115d152f2078bcf0e30b5652a6
rterminus <luizrenanpontes@gmail.com>
2026-09-24 16:47:50 -0300

    feat: Inventory adjustments

--- src/main/java/com/rpgadventure/equipment/Inventory.java
+++ src/main/java/com/rpgadventure/equipment/Inventory.java
@@ -5,41 +5,61 @@ import java.util.Map;
 
 public class Inventory<T> {
     private final int maxCapacity = 40;
-    private Map<String, T> items;
+    private Map<T, Integer> items;
 
     public Inventory() {
         this.items = new HashMap<>();
     }
 
     public void insertItem(T item) {
-        if (item == null)
-            throw new IllegalArgumentException("equipment.Item cannot be null.");
+        insertItem(item, 1);
+    }
 
-        if (items.size() < maxCapacity)
-            items.put(item.toString(), item);
+    public void insertItem(T item, Integer quantity) {
+        if (item == null)
+            throw new IllegalArgumentException("Item cannot be null.");
+
+        if (items.containsKey(item)) {
+            int currentQuantity = items.get(item);
+            items.put(item, currentQuantity + quantity);
+        } else {
+            if (items.size() < maxCapacity) {
+                items.put(item, quantity);
+            } else {
+                throw new IllegalStateException("Inventory is full.");
+            }
+        }
     }
 
     public void removeItem(T item) {
         if (item == null)
-            throw new IllegalArgumentException("equipment.Item cannot be null.");
+            throw new IllegalArgumentException("Item cannot be null.");
 
-        if (!items.containsKey(item.toString()))
-            throw new IllegalStateException("equipment.Item not in inventory.");
+        if (!items.containsKey(item))
+            throw new IllegalStateException("Item not in inventory.");
 
-        items.remove(item.toString());
+        items.remove(item);
+    }
+
+    public void removeItem(T item, Integer quantity) {
+        if (item == null)
+            throw new IllegalArgumentException("Item cannot be null.");
+
+        if (!items.containsKey(item))
+            throw new IllegalStateException("Item not in inventory.");
+
+        int currentQuantity = items.get(item);
+
+        if (quantity > currentQuantity) {
+            throw new IllegalStateException("Not enough quantity to remove.");
+        } else if (quantity == currentQuantity) {
+            items.remove(item);
+        } else {
+            items.put(item, currentQuantity - quantity);
+        }
     }
 
     public boolean hasItem(T item) {
-        return items.containsKey(item.toString());
-    }
-
-    @Override
-    public String toString() {
-        String stream = "";
-
-        for (T item : items.values())
-            stream += "equipment.Item name: " + item.toString() + "\n";
-
-        return stream;
+        return items.containsKey(item);
     }
 }
